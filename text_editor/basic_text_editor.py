56cac374027223c5e37f13ec4a068a6cf72c6daa
John Dell'Oso <john.delloso@gmail.com>
2024-06-28 17:31:38 +0930

    basic_text_editor.py: Added '__main__' check code.

--- text_editor/basic_text_editor.py
+++ text_editor/basic_text_editor.py
@@ -37,24 +37,25 @@ def save_file():
     window.title(f'Simple Text Editor - {filepath}')
 
 
-window = tk.Tk()
-window.title('Simple Text Editor')
+if __name__ == 'main':
+    window = tk.Tk()
+    window.title('Simple Text Editor')
 
-window.rowconfigure(0, minsize=800, weight=1)
-window.columnconfigure(1, minsize=800, weight=1)
+    window.rowconfigure(0, minsize=800, weight=1)
+    window.columnconfigure(1, minsize=800, weight=1)
 
-txt_edit = tk.Text(window)
-frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
-btn_open = tk.Button(frm_buttons, text='Open', command=open_file)
-btn_save = tk.Button(frm_buttons, text='Save As ...', command=save_file)
-btn_quit = tk.Button(frm_buttons, text='Quit', command=quit)
+    txt_edit = tk.Text(window)
+    frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+    btn_open = tk.Button(frm_buttons, text='Open', command=open_file)
+    btn_save = tk.Button(frm_buttons, text='Save As ...', command=save_file)
+    btn_quit = tk.Button(frm_buttons, text='Quit', command=quit)
 
-btn_open.grid(row=0, column=0, sticky='ew', padx=5, pady=5)
-btn_save.grid(row=1, column=0, sticky='ew', padx=5)
-btn_quit.grid(row=2, column=0, sticky='ew', padx=5, pady=5)
+    btn_open.grid(row=0, column=0, sticky='ew', padx=5, pady=5)
+    btn_save.grid(row=1, column=0, sticky='ew', padx=5)
+    btn_quit.grid(row=2, column=0, sticky='ew', padx=5, pady=5)
 
-frm_buttons.grid(row=0, column=0, sticky='ns')
-txt_edit.grid(row=0, column=1, sticky='nsew')
+    frm_buttons.grid(row=0, column=0, sticky='ns')
+    txt_edit.grid(row=0, column=1, sticky='nsew')
 
-# Run the application
-window.mainloop()
+    # Run the application
+    window.mainloop()
